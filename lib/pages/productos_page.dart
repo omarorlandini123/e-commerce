06346501e0247aeabbcd9ef01e3e4d0537ea2238
Omar Orlandini <omarorlandini123@gmail.com>
2018-07-14 03:10:11 -0500

    Productos propios y terceros demo listo

--- lib/pages/productos_page.dart
+++ lib/pages/productos_page.dart
@@ -42,53 +42,123 @@ class _ProductosPageState extends State<ProductosPage>
     with TickerProviderStateMixin {
   ListTabProductos lista;
   TabController _tabController;
+  bool mostrarBoton = true;
+
+  List<Widget> tarjetasLista(String image, String nombre, String costo) {
+    List<Widget> lista = new List<Widget>();
+    for (var i = 0; i < 15; i++) {
+      lista.add(tarjeta(image, nombre, costo));
+    }
+    return lista;
+  }
+
+  FloatingActionButton getFloatButton() {
+    if (mostrarBoton)
+      return FloatingActionButton(
+          elevation: 4.0,
+          child: new Icon(Icons.add),
+          backgroundColor: Colors.purple[700],
+          onPressed: () {});
+    else
+      return null;
+  }
+
+  Card tarjeta(String image, String nombre, String costo) {
+    return new Card(
+      elevation: 3.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(
+                top: 10.0, bottom: 10.0, left: 15.0, right: 15.0),
+            child: CircleAvatar(
+              radius: 40.0,
+              backgroundImage: new AssetImage(image),
+            ),
+          ),
+          Expanded(
+            child: Container(
+                margin: EdgeInsets.only(top: 15.0),
+                alignment: Alignment.centerLeft,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Expanded(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Material(
+                            child: Text(
+                          nombre,
+                          style: TextStyle(fontSize: 18.0),
+                        )),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Material(
+                            child: Text(
+                          costo,
+                          style: TextStyle(fontSize: 18.0),
+                        ))
+                      ],
+                    )),
+                    Container(
+                        alignment: Alignment.centerRight,
+                        margin: EdgeInsets.only(
+                            top: 10.0, bottom: 10.0, right: 15.0),
+                        child: IconButton(
+                          icon: Icon(Icons.share),
+                          onPressed: () {},
+                          iconSize: 30.0,
+                        ))
+                  ],
+                )),
+          )
+        ],
+      ),
+      margin: EdgeInsets.only(right: 20.0, left: 20.0, top: 10.0, bottom: 10.0),
+    );
+  }
 
   @override
   void initState() {
     super.initState();
     tabController = new TabController(length: 3, vsync: this);
+
     lista = new ListTabProductos();
     lista.addProducto(
-      new Tab(text: 'Propios'),
+      new Tab(
+        text: 'Propios',
+      ),
       new Container(
-        margin: EdgeInsets.only(top: 25.0, bottom: 25.0),
-        child: new ListView(
-          children: <Widget>[
-            new Card(
-              child: Text(
-                'Producto 1',
-                style: TextStyle(height: 5.0),
-              ),
-              margin: EdgeInsets.only(right: 20.0, left: 20.0),
-            ),
-            SizedBox(
-              height: 12.0,
-            ),
-            new Card(
-              child: Text(
-                'Producto 3',
-                style: TextStyle(height: 5.0),
-              ),
-              margin: EdgeInsets.only(right: 20.0, left: 20.0),
-            ),
-            SizedBox(
-              height: 12.0,
-            ),
-            new Card(
-              child: Text(
-                'Producto 2',
-                style: TextStyle(height: 5.0),
-              ),
-              margin: EdgeInsets.only(right: 20.0, left: 20.0),
-            )
-          ],
+        child: ListView(
+          children: tarjetasLista(
+              'assets/imgs/hamburguesa.jpg', "Hamburguesa", "Costo: S/10.00"),
         ),
       ),
     );
-    lista.addProducto(new Tab(text: 'Terceros'), new Container());
+    lista.addProducto(
+        new Tab(text: 'Terceros'),
+        new Container(
+          child: ListView(
+            children: tarjetasLista('assets/imgs/pollito.jpeg',
+                "Pollito a la Brasa", "Costo: S/24.00"),
+          ),
+        ));
 
     _tabController =
         new TabController(vsync: this, length: lista.getTabs().length);
+    _tabController.addListener(() {
+      this.setState(() {
+        if (_tabController.index == 1) {
+          mostrarBoton = false;
+        } else {
+          mostrarBoton = true;
+        }
+      });
+    });
   }
 
   @override
@@ -110,11 +180,7 @@ class _ProductosPageState extends State<ProductosPage>
         ),
         body: new TabBarView(
             controller: _tabController, children: lista.getElementos()),
-        floatingActionButton: FloatingActionButton(
-            elevation: 4.0,
-            child: new Icon(Icons.add),
-            backgroundColor: Colors.purple[700],
-            onPressed: () {}),
+        floatingActionButton: getFloatButton(),
         bottomNavigationBar: new Material(
           color: Colors.purple[900],
           child: new Container(

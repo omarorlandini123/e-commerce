cd929a3da33e6021f29ae4ac546645c795f8a0a1
Omar Orlandini <omarorlandini123@gmail.com>
2018-07-18 00:16:36 -0500

    Entidades de Menu listas

--- lib/entidades/OpcionMenu.dart
+++ lib/entidades/OpcionMenu.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
 
 class OpcionMenu {
+  Icon icono;
+  String etiqueta;
 
-  Icon _icono;
-  String _etiqueta;
-  
-  OpcionMenu(this._icono, this._etiqueta);
-
-  Icon get icono => _icono;
-  set icono(Icon icono) => _icono = icono;
-
-  String get etiqueta=>_etiqueta;
-  set etiqueta(String etiqueta) => _etiqueta=etiqueta;
-
+  OpcionMenu(this.icono, this.etiqueta);
 }
